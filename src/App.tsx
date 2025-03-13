5cb4dd8d5e8a4bd05ade64a1e937ef12dd03e580
Rajiv Roy <rajivpune18@gmail.com>
2025-03-13 18:51:41 +0530

    routing and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import About from "./About";
 import "./App.css";
 import Body from "./Body";
+import Login from "./Login";
+import Team from "./Team";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
-      <Body />
+      <a href="/">Home </a>
+      <a href="/about">About </a>
+      <a href="/team">Team </a>
+      <a href="/login">Login </a>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}></Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/team" element={<Team />}></Route>
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
